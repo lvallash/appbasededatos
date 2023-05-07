423a88b4b7d48d999d733d0a578db7cd8ea2ae0f
lvallash <leticiavh_3@hotmail.com>
2023-05-07 10:18:11 -0500

    update semana 6

--- src/main/java/pe/edu/cibertec/appbasededatos/controller/EstadoController.java
+++ src/main/java/pe/edu/cibertec/appbasededatos/controller/EstadoController.java
@@ -3,8 +3,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import pe.edu.cibertec.appbasededatos.model.bd.Estado;
 import pe.edu.cibertec.appbasededatos.service.EstadoService;
 
 @Controller
@@ -18,5 +21,26 @@ public class EstadoController {
 	public String frmMantEstado(Model model) {
 		model.addAttribute("listaEstado", estadoService.listarEstados());
 		return "Estado/frmMantEstado";
+	}
+	
+	@GetMapping("/frmRegEstado")
+	public String frmRegEstado(Model model) {
+		model.addAttribute("estadoForm", new Estado());
+		model.addAttribute("visualizar", false);
+		return "Estado/frmRegEstado";
+	}
+	
+	@PostMapping("/frmRegEstado")
+	public String registrarEstado(@ModelAttribute("estadoForm") Estado estado, Model model) {
+		String mensaje = "Estado registrado correctamente";
+		try {
+			estadoService.registrarEstado(estado);
+		} catch (Exception e) {
+			mensaje = "Estado no registrado";
+		}	
+		model.addAttribute("estadoForm", new Estado());
+		model.addAttribute("visualizar", true);
+		model.addAttribute("respuesta", mensaje);
+		return "Estado/frmRegEstado";
 	}
 }
--- src/main/java/pe/edu/cibertec/appbasededatos/model/bd/Estado.java
+++ src/main/java/pe/edu/cibertec/appbasededatos/model/bd/Estado.java
@@ -1,6 +1,8 @@
 package pe.edu.cibertec.appbasededatos.model.bd;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import lombok.Data;
@@ -12,6 +14,7 @@ import lombok.Data;
 public class Estado {
 	
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer idestado;
 	@Column (name = "descestado")
 	private String descestado;
